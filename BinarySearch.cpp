295d72da674a342f52f530bebcc774e46336f7b4
Modou Niang <43767534+Niangmodou@users.noreply.github.com>
2019-06-13 02:56:24 -0500

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -24,7 +24,7 @@ int binarySearch(int n, vector<int>lst){
 
 int main() {
     
-    vector<int>num = {1,2,3,4,5,6,7,8,9,10};
+    vector<int>num = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,22};
     int ind = binarySearch(6,num);
     cout << "Index of 6: "<<ind<<endl;
     
