5f50970965c5d593495dcf14255ed42a94a4c5be
Modou <niangmodou100@gmail.com>
2019-06-23 02:30:00 -0500

    Added randomize after each iteration

--- Playlist.cpp
+++ Playlist.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-//#include <ctime>
+#include <ctime>
 
 using namespace std;
 
@@ -60,7 +60,7 @@ public:
         vector<string>newList;
         int num;
         
-        //srand(time(NULL));
+        srand(time(NULL));
         
         
         while (newList.size() != songList.size()){
